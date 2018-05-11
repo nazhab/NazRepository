eed3f1cace2cb46b194250d9255d15b288e84fbb
Naz <naziathabib@yahoo.com>
2018-05-11 17:04:31 -0400

    Commit Again

---
+++ MyGit/src/com/mygit/java/MyGit2.java
@@ -0,0 +1,12 @@
+package com.mygit.java;
+
+public class MyGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome Again");
+
+	}
+
+}
