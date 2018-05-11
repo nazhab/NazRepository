532a1018ab15b280dadc7779f76f395ae1e9fabc
Naz <superman@justiceleague.com>
2018-05-11 16:52:42 -0400

    I commit

---
+++ MyGit/src/com/mygit/java/MyGit.java
@@ -0,0 +1,13 @@
+package com.mygit.java;
+
+public class MyGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+
+	}
+
+}
